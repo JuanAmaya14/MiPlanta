6e80d5ea117fdbc2284a0ee0fc3996f2991c2194
Amayap14 <juan.amaya.p14@gmail.com>
2026-03-14 15:22:30 -0500

    cartas de datos como componente

--- FRONTEND/MiPlantaFront/src/App.jsx
+++ FRONTEND/MiPlantaFront/src/App.jsx
@@ -1,25 +1,10 @@
 import "./App.css";
+import DatosTiempoReal from "./components/DatosTiempoReal.jsx";
 
 function App() {
   return (
     <>
-      <section className="seccionDatos">
-        <h1>Datos de la tierra en tiempo real.</h1>
-
-        <div className="card text-bg-light mb-3" style={{ maxWidth: "18rem" }}>
-          <h3 className="card-header">Temperatura</h3>
-          <div className="card-body">
-            <p className="display-1">20 &deg;C</p>
-          </div>
-        </div>
-
-        <div className="card text-bg-light mb-3" style={{ maxWidth: "18rem" }}>
-          <h3 className="card-header">Humedad</h3>
-          <div className="card-body">
-            <p className="display-1">60&#37;</p>
-          </div>
-        </div>
-      </section>
+      <DatosTiempoReal />
     </>
   );
 }
