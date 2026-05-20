cfc30d41ef86262064355dc31a72eedb74c044dd
Amayap14 <juan.amaya.p14@gmail.com>
2026-05-20 15:07:35 -0500

    arreglos en docker dev y titulo de la pagina

--- FRONTEND/MiPlantaFront/src/components/Footer.jsx
+++ FRONTEND/MiPlantaFront/src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <>
       <footer id="foot" className="text-center py-2 mt-auto">
-        <p>&copy; ola</p>
+        <p>&copy; ola 2026</p>
       </footer>
     </>
   );
--- FRONTEND/MiPlantaFront/vite.config.js
+++ FRONTEND/MiPlantaFront/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve';
+
+  return {
+    plugins: [react()],
+    server: isDev ? {
+      watch: {
+        usePolling: true,
+      },
+      host: true,
+      port: 5173,
+    } : {},
+  }
 })
